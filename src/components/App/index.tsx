c9a850b061d8cef5606cab296382bf3d042e8775
AleksMaifet <aleksmaifet@gmail.com>
2022-11-29 23:35:37 +0300

    add yml

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -1,22 +1,23 @@
 import React, { ReactElement, useState } from 'react';
 
-const DEFAULT_VALUE = 0
-const BUTTON_TITLE = 'Click'
+const DEFAULT_VALUE = 0;
+const BUTTON_TITLE = 'Click';
 
 const App = (): ReactElement => {
+  const [count, setCount] = useState<number>(DEFAULT_VALUE);
 
-  const [count,setCount] = useState<number>(DEFAULT_VALUE)
-
-  const handleCount = () => {
-    setCount(prev => prev+1)
-  }
+  const handleCount = (): void => {
+    setCount(prev => prev + 1);
+  };
 
   return (
     <>
       <h1>{count}</h1>
-      <button onClick={handleCount}>{BUTTON_TITLE}</button>
+      <button type="button" onClick={handleCount}>
+        {BUTTON_TITLE}
+      </button>
     </>
   );
-}
+};
 
 export default App;
